90d0ea47dac8e1e44f27666fec35981338c2d289
Mouadamtti <158284959+Mouadamtti@users.noreply.github.com>
2024-06-14 00:04:52 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,93 +1,98 @@
-import jimp from 'jimp';
-import pkg from '@whiskeysockets/baileys';
-const { generateWAMessageFromContent, proto, prepareWAMessageMedia } = pkg;
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
+    const d = new Date(new Date + 3600000);
+    const locale = 'es-ES';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: '2-digit', month: '2-digit', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `Zall - 𝘽𝙊𝙏
 
-let handler = async (m, { conn, usedPrefix, __dirname, text, isPrems }) => {
-    if (usedPrefix == 'a' || usedPrefix == 'A') return;
-    try {
-        let senderTag = m.pushName || conn.getName(m.sender);
-        let imageBuffer = await genProfile(); // Generate image without passing conn or m
-        m.react('🔥');
+ ▢ *Hola,* ${taguser}
 
-        const buttonParamsJson = JSON.stringify({
-            title: "Show options",
-            description: "Get information through official means about zall",
-            sections: [
-                {
-                    title: "Zall more ", highlight_label: "Popular",
-                    rows: [
-                        { header: "Account Commands", title: "", description: "all account commands", id: usedPrefix + "accmenu" }
-                    ]
-                },
-                {
-                    title: "Zall downloads", highlight_label: "Popular",
-                    rows: [
-                        { header: "Download Commands", title: "", description: "All download commands", id: usedPrefix + "downmenu" }
-                    ]
-                },
-                {
-                    title: "Zall Ai ", highlight_label: "Popular",
-                    rows: [
-                        { header: "Ai Commands", title: "", description: "all ai commands", id: usedPrefix + "aimenu" }                        
-                    ]
-                },
-                {
-                    title: "Zall Other ", highlight_label: "Popular",
-                    rows: [
-                        { header: "Other Commands", title: "", description: "all Other commands", id: usedPrefix + "menuother" },
-                        { header: "info", title: "", description: "Info user", id: usedPrefix + "info" }
-                    ]
-                },
-                {
-                    title: "Ⓜ️ Menu", highlight_label: "Popular",
-                    rows: [
-                        { header: "⭐ Full Menu", title: "", description: "Visit all the commands", id: usedPrefix + "allmenu" }
-                    ]
-                }
-            ]
-        });
+ _*< TUS ESTADÍSTICAS />*_
 
-        const interactiveMessage = {
-            body: { text: `Hello 👋, ${senderTag}` },
-            footer: { text: "@mouad_78" },
-            header: {
-                hasMediaAttachment: true,
-                ...await prepareWAMessageMedia({
-                    image: imageBuffer // Use the generated image buffer
-                }, {
-                    upload: conn.waUploadToServer
-                })
-            },
-            nativeFlowMessage: {
-                buttons: [{
-                    name: "single_select",
-                    buttonParamsJson
-                }]
-            }
-        };
+ ▢ *Nivel:* ${level}
+ ▢ *Experiencia:* ${exp}
+ ▢ *Rango:* ${role}
+ ▢ *Gold:* ${limit}
+ ▢ *Premium:* ${user.premiumTime > 0 ? '✅' : (isPrems ? '✅' : '❌') || ''}
+  
+┏━━⊜  _*< Bot Commnds />*_
+┃⋄ /alive
+┃⋄ /jadibot
+┃⋄ /serbor
+┃⋄ /qr
+┃⋄ /getcode
+┃⋄ /owner
+┃⋄ /ping
+┃⋄ /runtime
+┃⋄ /rentbot
+┗━━━━━━━━⬣
+┏━━⊜ *< Premium Commnds />*_
+┃⋄ /apk
+┃⋄ /apk2
+┃⋄ /uptodown
+┗━━━━━━━━⬣
 
-        const message = {
-            messageContextInfo: { deviceListMetadata: {}, deviceListMetadataVersion: 2 },
-            interactiveMessage
-        };
+┏━━.*<Ai Commnds/>*
+┃⋄ /aibobiza
+┃⋄ /aipilot
+┃⋄ /bardai
+┃⋄ /bardi
+┃⋄ /hd
+┗━━━━━━━━⬣
 
-        await conn.relayMessage(m.chat, { viewOnceMessage: { message } }, {});
-    } catch (e) {
-        console.log(e);
+┏━━⊜ *< downloade Commnds />*
+┃⋄ /capcut
+┃⋄ /facebook3
+┃⋄ /insta
+┃⋄ /mp4
+┃⋄ /snapsave
+┃⋄ /song
+┃⋄ /tiktok2
+┃⋄ /twitter
+┃⋄ 
+┃⋄ 
+┃⋄ 
+┗━━━━━━━━⬣
+
+`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
+  } catch {
+    conn.reply(m.chat, '*[ ℹ️ ] Este menu tiene un error interno, por lo cual no fue posible enviarlo.*', m);
+  }
 };
-
-handler.command = /^(menu)$/i;
+handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i;
+handler.exp = 50;
+handler.fail = null;
 export default handler;
-
-async function genProfile() {
-    try {
-        // Generate an image using JIMP
-        const image = await jimp.read('https://telegra.ph/file/25138d52d9359a658c727.jpg'); // Use your image URL
-        image.resize(256, 256); // Resize the image if necessary
-        return await image.getBufferAsync(jimp.MIME_JPEG); // Return the image buffer
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
 }
